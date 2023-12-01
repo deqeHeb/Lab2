68f25f024d639ad49acbac91701693162d4308e1
ivanova.d.i <hohobu3@gmail.com>
2023-12-02 01:14:02 +0300

    Добавление функций для всех операций

--- funcs.c
+++ funcs.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void CheckString(char *str, char *num1, char *num2, char *oper)
+void CheckString(char *str, char *num1, char *num2,char *oper)
 {
     char alf[] = "x0123456789AaBbCcDdEeFf";
     char alfOper[] = "+-*%^&|";
@@ -31,7 +31,7 @@ void CheckString(char *str, char *num1, char *num2, char *oper)
         }
     }
 
-    if (count_space == 1 || count_space > 2 )
+    if (count_space == 1 || count_space > 2 || (str[0] != '~' && count_space == 0))
     {
 	printf("Ошибка: неправильная расстановка пробелов\n");
 	exit(-1);
@@ -47,7 +47,9 @@ void CheckString(char *str, char *num1, char *num2, char *oper)
 		if (alf[j] == str[i])
 		{
 		    flag = 1;
-		    num1[strlen(num1)] = str[i];
+		    num1[ind1] = str[i];
+		    ind1++;
+		    num1[ind1] = '\0';
 		}
 	    }
 	    if (flag == 0)
@@ -72,7 +74,7 @@ void CheckString(char *str, char *num1, char *num2, char *oper)
 		    flag = 1;
 		    break;
 		}
-                if (i != 0 && str[i-1] == ' ')
+                if (i != 0 && str[i] == '-' && str[i-1] == ' ')
 		{
 		    num2[ind2] = '-';
 		    ind2++;
@@ -137,3 +139,332 @@ void CheckString(char *str, char *num1, char *num2, char *oper)
         }
     }
 }
+
+int GetSS(char *str) //Получение системы счисления числа
+{
+    int len = strlen(str);
+    char alf2[] = "01";
+
+    char alf8[] = "01234567";
+
+    char alf16[] = "0123456789AaBbCcDdEeFf";
+    int flag = 0;
+
+    int k = 0;
+
+    if (str[0] == '-')
+    {
+	k = 1;
+    }
+
+    if (str[k] == '0' && str[k+1] == 'x')
+    {
+	for (int i = k+2; i < len; i++)
+	{
+	    flag = 0;
+            for (int j = 0; j < strlen(alf16); j++)
+	    {
+		if (str[i] == alf16[j])
+		{
+		    flag = 1;
+		}
+	    }
+	    if (!flag) break;
+	    if (i == len-1) return 16;
+	}
+    }
+    else if (str[k] == '0' && len > 1+k)
+    {
+        for (int i = k+1; i < len; i++)
+	{
+	    flag = 0;
+            for (int j = 0; j < strlen(alf8); j++)
+	    {
+		if (str[i] == alf8[j])
+		{
+	            flag = 1;
+		}
+	    }
+	    if (flag == 0) break;
+	    if (i == len-1) return 8;
+	}
+    }
+    else 
+    {
+        for (int i = k; i < len; i++)
+	{
+	    flag = 0;
+            for (int j = 0; j < strlen(alf2); j++)
+	    {
+		if (str[i] == alf2[j])
+		{
+		    flag = 1;
+		}
+	    }
+	    if (!flag) break;
+	    if (i == len-1) return 2;
+	}
+    }
+    return 0;
+}
+
+int GetDig(char c) //Перевод цифры (0-F) в десятичное число
+{
+    switch (c)
+    {
+	case '0':
+	    return 0;
+	case '1':
+	    return 1;
+	case '2':
+	    return 2;
+        case '3':
+	    return 3;
+	case '4':
+	    return 4;
+	case '5':
+	    return 5;
+	case '6':
+	    return 6;
+	case '7':
+	    return 7;
+	case '8':
+	    return 8;
+	case '9':
+	    return 9;
+        case 'A':
+	    return 10;
+	case 'a':
+	    return 10;
+	case 'B':
+	    return 11;
+	case 'b':
+	    return 11;
+        case 'C':
+	    return 12;
+	case 'c':
+	    return 12;
+	case 'D':
+	    return 13;
+	case 'd':
+	    return 13;
+	case 'E':
+	    return 14;
+	case 'e':
+	    return 14;
+	case 'F':
+	    return 15;
+        case 'f':
+	    return 15;
+	default:
+	    return 0;
+    }
+}
+
+char GetChar(int c) //Перевод числа (остатка от деления) в цифру(0-F) 
+{
+    switch (c)
+    {
+	case 0:
+	    return '0';
+	case 1:
+	    return '1';
+	case 2:
+	    return '2';
+        case 3:
+	    return '3';
+	case 4:
+	    return '4';
+	case 5:
+	    return '5';
+	case 6:
+	    return '6';
+	case 7:
+	    return '7';
+	case 8:
+	    return '8';
+	case 9:
+	    return '9';
+        case 10:
+	    return 'a';
+	case 11:
+	    return 'b';
+	case 12:
+	    return 'c';
+	case 13:
+	    return 'd';
+        case 14:
+	    return 'e';
+	case 15:
+	    return 'f';
+	default:
+	    return ' ';
+    }
+}
+
+
+long poww(long a, long b) //Возведение в степень
+{
+    long res = 1;
+    for (int i = 0; i < b; i++)
+    {
+	res *= a;
+    }
+    return res;
+}
+
+int StrToDec(char *str, int ss) //Перевод строки в десятичную СС
+{
+    int res = 0;
+    int i = 0;
+    int sign = 1;
+
+    if (str[0] == '-')
+    {
+	i = 1;
+	sign = -1;
+    } 
+    
+    if (ss == 16) i += 2;
+    else if (ss == 8) i += 1;
+    int p = strlen(str)-1-i;
+
+    for (i; i < strlen(str); i++)
+    {
+	res += GetDig(str[i])*poww(ss, p);
+	p--;
+    }
+
+    return res*sign;
+}
+
+char *IntToStr(int num, int ss) //Перевод из десятичного числа в ss-чную СС в виде строки
+{
+    int d = num;
+    char *str = (char*)malloc(500 * sizeof(char));
+
+    if (str == NULL)
+    {
+	printf("Ошибка: null malloc");
+	exit(-1);
+    }
+    int indStr = 0;  
+
+    if (num < 0)
+    {
+	d *= (-1);
+    }
+    
+    while (d > 0) //Перевод числа из десятичной в ss-чную систему счисления
+    {
+	str[indStr] = GetChar(d % ss);
+	indStr++;
+	str[indStr] = '\0';
+	
+	d /= ss;
+    }
+
+    if (strlen(str) == 0)
+    {
+        str[indStr] = '0';
+	indStr++;
+	str[indStr] = '\0';
+    }
+
+    if (ss == 16)
+    {
+	str[indStr] = 'x';
+	indStr++;
+        str[indStr] = '0';
+	indStr++;
+	str[indStr] = '\0';
+
+    }
+    else if (ss == 8)
+    {
+	str[indStr] = '0';
+	indStr++;
+	str[indStr] = '\0';
+    }
+
+    if (num < 0) //Если число отрицательное, добавляем минус
+    {
+	str[indStr] = '-';
+	indStr++;
+	str[indStr] = '\0';
+    }
+
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) //Переворачиваем строку 
+    {
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+
+    return str;
+}
+
+void PrintRes(int res, int ss)
+{
+    char *resStr = IntToStr(res, ss);
+    printf("%s (%d)\n", resStr, res);
+    free(resStr);
+}
+
+int Addition(char *num1, char *num2, int ss)
+{
+    int res;
+    res = StrToDec(num1, ss) + StrToDec(num2, ss); 
+    return res;
+}
+
+int Subtr(char *num1, char *num2, int ss)
+{
+    int res;
+    res = StrToDec(num1, ss) - StrToDec(num2, ss); 
+    return res;
+}
+
+int Mult(char *num1, char *num2, int ss)
+{
+    int res;
+    res = StrToDec(num1, ss) * StrToDec(num2, ss);
+    return res;
+}
+
+int Divis(char *num1, char *num2, int ss)
+{
+    int res;
+    res = StrToDec(num1, ss) % StrToDec(num2, ss); 
+    return res;
+}
+
+int BitAnd(char *num1, char *num2, int ss)
+{
+    int res;
+    res = StrToDec(num1, ss) & StrToDec(num2, ss); 
+    return res;
+}
+
+int BitOr(char *num1, char *num2, int ss)
+{
+    int res;
+    res = StrToDec(num1, ss) | StrToDec(num2, ss); 
+    return res;
+}
+
+int BitXor(char *num1, char *num2, int ss)
+{
+    int res;
+    res = StrToDec(num1, ss) ^ StrToDec(num2, ss); 
+    return res;
+}
+
+int BitNot(char *num1, int ss)
+{
+    int res;
+    res = ~(StrToDec(num1, ss)); 
+    return res;
+}
+
--- main.c
+++ main.c
@@ -9,7 +9,10 @@ int main()
 {
     size_t size = 500;
 
-    char *str = (char*)malloc(sizeof(char)*size);
+    char *str;
+
+    printf("Выражение: ");
+    size_t line = getline(&str, &size, stdin);
 
     if (str == NULL)
     {
@@ -17,16 +20,63 @@ int main()
 	exit(-1);
     }
 
-    printf("Выражение: ");
-    size_t line = getline(&str, &size, stdin);
 
     char num1[size];
     char num2[size];
     char oper = 'a';
 
     CheckString(str, num1, num2, &oper);
-    printf("n1: %s, n2: %s, oper: %c", num1, num2, oper);
+    //printf("n1: %s, n2: %s, oper: %c\n", num1, num2, oper);
 
+    int ss1 = GetSS(num1);
+    int ss2 = GetSS(num2);
+
+    //printf("%d %d\n", ss1, ss2);
+
+    if (ss1 == 0 || (ss2 == 0 && oper != '~'))
+    {
+	printf("Ошибка: неправильная система счисления\n");
+	exit(-1);
+    }
+    if (ss1 != ss2 && oper != '~')
+    {
+	printf("Ошибка: разные системы счисления\n");
+	exit(-1);
+    }
+
+    int res;
+
+    switch(oper)
+    {
+	case '+':
+	    res = Addition(num1, num2, ss1);
+	    break;
+	case '-':
+	    res = Subtr(num1, num2, ss1);
+	    break;
+	case '*':
+	    res = Mult(num1, num2, ss1);
+	    break;
+	case '%':
+	    res = Divis(num1, num2, ss1);
+	    break;
+	case '&':
+	    res = BitAnd(num1, num2, ss1);
+	    break;
+	case '|':
+	    res = BitOr(num1, num2, ss1);
+	    break;
+	case '^':
+	    res = BitXor(num1, num2, ss1);
+	    break;
+	case '~':
+	    res = BitNot(num1, ss1);
+	    break;
+	default:
+	    break;
+    }
+
+    PrintRes(res, ss1);
     free(str);
 
     return 0;
